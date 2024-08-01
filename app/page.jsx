deed5380b081ead455679631003ead5b5b034db5
Nico Lee <new.nikolee2007@gmail.com>
2024-08-01 19:30:05 +0700

    homepage 20%

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,33 @@ import {FiDownload} from "react-icons/fi"
 const Home = () => {
   return (
     <section className="h-full">
-      <div className="container mx-auto">homepage</div>
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        {/*text*/}
+        <div className="text-center xl:text-left">
+          <span className="text-xl">Software Developer</span>
+          <h1 className="h1 mb-6">
+            Hello I'm <br /> <span className="text-accent">Nico Lee N.H</span>
+          </h1>
+
+          <p className="max-w-[500px] mb-9 text-white">
+            I student at SMK Telkom Malang
+          </p>
+
+          {/* btn and socials */}
+          <div className="flex flex-col xl:flex-row items-center gap-8">
+            <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+              <span>Download my CV</span>
+              <FiDownload className="text-xl" />
+            </Button>
+            <div>socials</div>
+          </div>
+          </div>
+        
+        {/*photo*/}
+        <div>photo</div>
+        </div>
+        </div>
     </section>
   )
 }
