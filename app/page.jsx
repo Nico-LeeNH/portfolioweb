5a28428707787b336a5cac5b6f1ff51957bff85a
Nico Lee <new.nikolee2007@gmail.com>
2024-08-02 17:10:49 +0700

    homepage 70%

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button"
 import {FiDownload} from "react-icons/fi"
 
+
+//components
+import Social from "@/components/Social"
+import Photo from "@/components/Photo"
+
 const Home = () => {
   return (
     <section className="h-full">
@@ -23,12 +28,16 @@ const Home = () => {
               <span>Download my CV</span>
               <FiDownload className="text-xl" />
             </Button>
-            <div>socials</div>
+            <div className="mb-8 xl:mb-0">
+              <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+            </div>
           </div>
           </div>
         
         {/*photo*/}
-        <div>photo</div>
+        <div>
+          <Photo />
+        </div>
         </div>
         </div>
     </section>
