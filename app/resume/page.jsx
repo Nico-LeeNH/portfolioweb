608a5294bc71c1612c7d920357e33525431891f0
Nico Lee <new.nikolee2007@gmail.com>
2024-08-08 20:06:19 +0700

    resume on progress

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,45 @@
+"use client"
+
+import {FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs} from 'react-icons/fa'
+import {SiTailwindcss, SiNextdotjs} from 'react-icons/si'
+
+
+// about
+const about ={
+  title: "About me",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Nico Lee "
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "+6281-55599-2978"
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "3+ Years"
+    },
+    {
+      fieldName: "Country",
+      fieldValue: "Indonesia"
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "nathanaelhandokonico@gmail.com"
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available"
+    },
+    {
+      fieldName: "Language",
+      fieldValue: "Indo, English, Chinese"
+    }
+   ]
+}
+
 const Resume = () => {
   return (
     <div>Resume page</div>
