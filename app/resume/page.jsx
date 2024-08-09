5b05f831b64aaec05c7f2b75779886bbe89f2ad0
Nico Lee <new.nikolee2007@gmail.com>
2024-08-09 20:56:59 +0700

    resume page on progress

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -111,7 +111,29 @@ import { motion } from 'framer-motion';
 
 const Resume = () => {
   return (
-    <div>Resume page</div>
+    <motion.div
+    initial = {{ opacity: 0 }}
+    animate={{ 
+      opacity: 1,
+      transition: { delay: 2.4, duration: 0.4, ease: "easeIn"},
+     }}
+     className='min-h-[80vh] flex items-center justify-center py-12 xl:py-0'
+    >
+      <div className="container mx-auto">
+        <Tabs defaultValue="experience"
+        className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList>
+            <TabsTrigger>Experience</TabsTrigger>
+            <TabsTrigger>Education</TabsTrigger>
+            <TabsTrigger>Skills</TabsTrigger>
+            <TabsTrigger>About me</TabsTrigger>
+          </TabsList>
+
+          {/*content*/}
+          <div>content</div>
+        </Tabs>
+      </div>
+    </motion.div>
   )
 }
 
