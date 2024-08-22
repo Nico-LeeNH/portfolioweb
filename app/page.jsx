f4f87aeb527035074e2ac71d455de7e8c0d4d7b5
Nico Lee <new.nikolee2007@gmail.com>
2024-08-22 21:15:27 +0700

    update cv

--- app/page.jsx
+++ app/page.jsx
@@ -25,10 +25,12 @@ const Home = () => {
 
           {/* btn and socials */}
           <div className="flex flex-col xl:flex-row items-center gap-8">
+            <a href="./CV Nico Lee Nathanael Handoko.pdf" target="_blank" alt="my cv" >
             <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
               <span>Download my CV</span>
               <FiDownload className="text-xl" />
             </Button>
+              </a>
             <div className="mb-8 xl:mb-0">
               <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
             </div>
