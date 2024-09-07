fe0860d283551cab1af063332053b6bfe0d9b18a
Nico Lee <new.nikolee2007@gmail.com>
2024-09-07 10:04:42 +0700

    update project page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -8,72 +8,107 @@ import "swiper/css";
 
 import { BsArrowUp, BsArrowUpRight, BsGithub } from "react-icons/bs";
 
-import{
+import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
-
+} from "@/components/ui/tooltip";
 
 import Link from "next/link";
 import Image from "next/image";
 import WorkSliderBtns from "@/components/WorkSliderBtns";
 
-
 const projects = [
   {
     num: "01",
-    category: "MedTrack",
+    category: "Web Portfolio",
     title: "project 1",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit minus nisi corrupti numquam dolorem, odio nulla temporibus eligendi officiis placeat neque.",
-    stack:[{name: "Html 5"}, {name: "Css 3"}, {
-      name: "javascript"
-    }],
-    image: '/assets/work/MedShop.png',
-    live: "",
-    github: "https://github.com/Nico-LeeNH/MedShop/tree/3a6e1f6354b66ec2f5591616f45ff7ce8b6ef4c7/Medshop/Medtrack",
+    description:
+      "Ini adalah Web Portfolio saya menggunakan React, Next.js dan Talwind CSS",
+    stack: [
+      { name: "React" },
+      { name: "Next.js" },
+      {
+        name: "Talwind CSS",
+      },
+    ],
+    image: "/assets/work/image.png",
+    live: "https://nicolee.vercel.app",
+    github: "https://github.com/Nico-LeeNH/portfolioweb",
   },
   {
     num: "02",
-    category: "frontend",
+    category: "MedTrack",
     title: "project 1",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit minus nisi corrupti numquam dolorem, odio nulla temporibus eligendi officiis placeat neque.",
-    stack:[{name: "Html 5"}, {name: "Css 3"}, {
-      name: "javascript"
-    }],
-    image: '/assets/work/thumb2.png',
+    description:
+      "Project ini dibangun menggunakan Bootstrap, CSS, PHP, dan JavaScript. MedTrack berfungsi untuk memantau kesehatan pasien dengan menyediakan platform yang mudah digunakan dan intuitif.",
+    stack: [
+      { name: "Bootstrap" },
+      { name: "CSS" },
+      { name: "PHP" },
+      {
+        name: "Javascript",
+      },
+    ],
+    image: "/assets/work/MedShop.png",
     live: "",
-    github: "",
+    github:
+      "https://github.com/Nico-LeeNH/MedShop/tree/3a6e1f6354b66ec2f5591616f45ff7ce8b6ef4c7/Medshop/Medtrack",
   },
   {
     num: "03",
     category: "frontend",
     title: "project 1",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit minus nisi corrupti numquam dolorem, odio nulla temporibus eligendi officiis placeat neque.",
-    stack:[{name: "Html 5"}, {name: "Css 3"}, {
-      name: "javascript"
-    }],
-    image: '/assets/work/thumb3.png',
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit minus nisi corrupti numquam dolorem, odio nulla temporibus eligendi officiis placeat neque.",
+    stack: [
+      { name: "Html 5" },
+      { name: "Css 3" },
+      {
+        name: "javascript",
+      },
+    ],
+    image: "/assets/work/thumb2.png",
     live: "",
     github: "",
-  }
-]
+  },
+  {
+    num: "04",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit minus nisi corrupti numquam dolorem, odio nulla temporibus eligendi officiis placeat neque.",
+    stack: [
+      { name: "Html 5" },
+      { name: "Css 3" },
+      {
+        name: "javascript",
+      },
+    ],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
 
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
-  
+
   const handleSlideChange = (swiper) => {
     // get current slide index
     const currentIndex = swiper.activeIndex;
     // update project state based on current slide index
     setProject(projects[currentIndex]);
-  }
+  };
   return (
     <motion.section
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"}, }}
-    className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
@@ -84,18 +119,21 @@ const Work = () => {
                 {project.num}
               </div>
               {/*project category*/}
-              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">{project.category} project</h2>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
               {/*project description*/}
               <p className="text-white/60">{project.description}</p>
               {/*stack*/}
               <ul className="flex gap-4">
-                {project.stack.map((item, index)=>{
-                  return(
-                    <li key={index} className="text-xl text-accent">{item.name}
-                    {/*remove the last commo*/}
-                    {index !== project.stack.length - 1 && ","}
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {/*remove the last commo*/}
+                      {index !== project.stack.length - 1 && ","}
                     </li>
-                  )
+                  );
                 })}
               </ul>
               {/*border*/}
@@ -104,10 +142,10 @@ const Work = () => {
               <div className="flex items-center gap-4">
                 {/*live project button*/}
                 <Link href={project.live}>
-                <TooltipProvider delayDuration={100}>
+                  <TooltipProvider delayDuration={100}>
                     <Tooltip>
                       <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                      <BsArrowUpRight className="text-white text-3xl group-hover:text-accent"/>
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
                       </TooltipTrigger>
                       <TooltipContent>
                         <p>Live Project</p>
@@ -120,7 +158,7 @@ const Work = () => {
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
                       <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                      <BsGithub className="text-white text-3xl group-hover:text-accent"/>
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
                       </TooltipTrigger>
                       <TooltipContent>
                         <p>Github repository</p>
@@ -132,41 +170,44 @@ const Work = () => {
             </div>
           </div>
           <div className="w-full xl:w-[50%]">
-          <Swiper
-          spaceBetween={30}
-          slidesPerView={1}
-          className="xl:h-[520px] mb-12"
-          onSlideChange={handleSlideChange}
-          >
-                {projects.map((project, index) =>{
-                  return <SwiperSlide key={index} className="w-full">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
                     <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                    {/*overlay*/}
-                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      {/*overlay*/}
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
 
-                    {/*image*/}
-                    <div className="relative w-full h-full">
-                      <Image 
-                      src={project.image}
-                      fill
-                      className="object-cover"
-                      alt=""
-                      />
+                      {/*image*/}
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          alt=""
+                        />
+                      </div>
                     </div>
-                    </div>
-                  </SwiperSlide>;
-                })}
+                  </SwiperSlide>
+                );
+              })}
 
-                {/*slider button*/}
-                <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+              {/*slider button*/}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
                 btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
-                />
-              </Swiper>
+              />
+            </Swiper>
           </div>
         </div>
       </div>
     </motion.section>
-  )
-}
+  );
+};
 
 export default Work;
